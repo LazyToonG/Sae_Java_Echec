c47709a4007c23d415480abc5158f9e20b20c157
LazyToonG <arthur.shishkovskiy@gmail.com>
2025-05-26 13:40:58 +0200

    initialisation

---
+++ Pion.java
@@ -0,0 +1,10 @@
+public class Pion extends Piece{
+    
+    public pion (int x, int y)  {
+        super(x, y);
+    }
+
+    public boolean avanceDeDeux (int x) {
+        if (x.equal())
+    }
+}
