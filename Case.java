fa61d205361deb7cb8ba82c8a8091359d5207945
LazyToonG <arthur.shishkovskiy@gmail.com>
2025-06-03 12:06:01 +0200

    avancement sur la class case et echiquier

--- Case.java
+++ Case.java
@@ -3,16 +3,30 @@ public class Case {
     //Atributs
     private int positionX;
     private int positionY;
+    private Piece piece;
 
     //Constructeur
     public Case (int positionX, int positionY) {
         this.positionX = positionX;
         this.positionY = positionY;
+        this.piece = null;
     }
 
     //Methodes
-    public String caseActuelle() {
-        return positionX + " " + positionY;
+    
+
+
+
+
+
+
+
+    public boolean caseActuelle(Piece p) {
+        if (p == null){
+            this.contientPiece = false;
+        } else {
+            this.contentpiece = true;
+        }
     }
 
     public void caseChoisie (int positionX, int positionY) {
@@ -35,6 +49,10 @@ public class Case {
         this.positionY = positionY;
     }
 
+    public void setPiece(Piece piece){
+		this.piece = piece;
+	}
+
     //Getter
     public int getPositionX () {
         return positionX;
@@ -42,6 +60,23 @@ public class Case {
 
     public int getPositionY () {
         return positionY;
+    }
+
+    public Piece getPiece () {
+        return piece;
+    }
+
+
+
+
+
+
+
+
+
+
+    public boolean contientPiece(){
+        return contientPiece;
     }
 
 }
