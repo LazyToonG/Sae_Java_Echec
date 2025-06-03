2e59e175b913d941fa32356363ffca0f762a85cc
LazyToonG <arthur.shishkovskiy@gmail.com>
2025-06-03 12:04:49 +0200

    avancement sur la class case et echiquier

--- Echiquier.java
+++ Echiquier.java
@@ -1,30 +1,46 @@
 public class Echiquier {
 
-    private int ligne;
-    private int colonne;
+    private Case[][] echiquier;
 
-    public Echiquier (int ligne, int colonne) {
-        this.ligne = ligne;
-        this.colonne = colonne;
-    }
-
-    public void int plateauEchec () {
-        for (int  i=0; i<9; i++) {
-
-            ligne += 1;
-            
-            System.out.println(T,C,F,R,Roi,F,C,T);
-            System.out.println(P,P,P,P,P,P,P,P);
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println(P,P,P,P,P,P,P,P);
-            System.out.println(T,C,F,Roi,R,F,C,T);
-
+    public Echiquier (Case[][] echiquier) {
+        this.echiquier = echiquier;
+        for (int i = 0; i< 8; i++){
+            for (int y = 0; y < 8; y++) {
+                echiquier[i][y] = new Case(i,y);
+            }
+            echiquier[0][0].setPiece(new Tour());
+            echiquier[0][1].setPiece(new Cavalier);
+            echiquier[0][2].setPiece(new Four);
+            echiquier[0][3].setPiece(new Dame);
+            echiquier[0][4].setPiece(new Roi);
+            echiquier[0][5].setPiece(new Fou);
+            echiquier[0][6].setPiece(new Cavalier);
+            echiquier[0][7].setPiece(new Tour);
+            for (int i = 0; i < 8; i++) {
+                echiquier[1][i].setPiece(new Pion());
+            }
+            echiquier[8][0].setPiece(new Tour());
+            echiquier[8][1].setPiece(new Cavalier);
+            echiquier[8][2].setPiece(new Four);
+            echiquier[8][3].setPiece(new Dame);
+            echiquier[8][4].setPiece(new Roi);
+            echiquier[8][5].setPiece(new Fou);
+            echiquier[8][6].setPiece(new Cavalier);
+            echiquier[8][7].setPiece(new Tour);
+            for (int i = 0; i < 8; i++) {
+                echiquier[7][i].setPiece(new Pion());
+            }
         }
 
     }
 
-    
+    public Case[][] getEchiquier (){
+        return echiquier;
+    }
+
+		
 }
+
+
+
+    
