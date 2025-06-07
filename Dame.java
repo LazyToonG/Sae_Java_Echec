b5419f7115e3dcb5c83ac21b0cfbed1af7d7ec52
LazyToonG <arthur.shishkovskiy@gmail.com>
2025-06-07 16:08:51 +0200

    methode deplacement pour pion, dame et four

--- Dame.java
+++ Dame.java
@@ -1,8 +1,25 @@
 public class Dame extends Pion{
 
-    public boolean deplacement (Case destination) {
+    public Dame (String couleur, Case destination) {
+		super(couleur, destination);
+	}
 
-    }
 
-
+    public boolean deplacement(Case destination){
+    	for(int i=0; i < 8; i++){
+    		if(destination.getPositionX()==this.getDestination().getPositionX()+i && destination.getPositionY()==this.getDestination().getPositionY()+i){
+    			return true;}
+			if(destination.getPositionX()==this.getDestination().getPositionX()+i && destination.getPositionY()==this.getDestination().getPositionY()-i){
+				return true;}
+			if(destination.getPositionX()==this.getDestination().getPositionX()-i && destination.getPositionY()==this.getDestination().getPositionY()+i){
+				return true;}
+			if(destination.getPositionX()==this.getDestination().getPositionX()-i && destination.getPositionY()==this.getDestination().getPositionY()-i){
+				return true;}
+            if(destination.getPositionX()==this.getDestination().getPositionX()+1 || destination.getPositionX()==this.getDestination().getPositionX()-1){
+                return true;}
+            if(destination.getPositionY()==this.getDestination().getPositionY()+1 || destination.getPositionY()==this.getDestination().getPositionY()-1){
+                return true;}		
+			}
+    		return false;
+		}
 }
